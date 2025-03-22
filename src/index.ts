74eb46e4140c86353cf91154091dcfa38fa27ab8
NathanFallet <contact@nathanfallet.me>
2025-03-22 18:42:20 +0100

    fixing

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import {Model, ModelAttributeColumnOptions} from "sequelize";
+import {DataTypes, Model, ModelAttributeColumnOptions} from "sequelize"
 import {parse, stringify, v4} from "uuid"
 import {Nullable} from "better-nullable";
 
 const uuidDatatype = <M extends Model>(options: Omit<ModelAttributeColumnOptions<M>, "type"> = {}): ModelAttributeColumnOptions<M> => ({
     ...options,
-    type: "BINARY(16)",
+    type: DataTypes.BLOB("tiny"),
     defaultValue: () => Buffer.from(parse(v4())),
     get() {
-        const value: Nullable<Uint8Array> = this.getDataValue('id');
-        return value ? stringify(value) : null;
+        const value: Nullable<Buffer> = this.getDataValue("id")
+        return value ? stringify(value) : null
     },
     set(value) {
-        if (typeof value !== 'string') return
-        this.setDataValue('id', Buffer.from(parse(value)));
+        if (typeof value !== "string") return
+        this.setDataValue("id", Buffer.from(parse(value)))
     },
 })
 
